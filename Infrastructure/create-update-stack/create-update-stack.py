3bde824df85ca521e4328f79ab36f2956d3efaae
shivakrishna <shivakrishna.ramadugu@gmail.com>
2017-12-23 15:19:45 -0600

    changed parameters

--- Infrastructure/create-update-stack/create-update-stack.py
+++ Infrastructure/create-update-stack/create-update-stack.py
@@ -8,32 +8,34 @@ parser.add_argument('--filename', type=str, help='Provide the filename')
 parser.add_argument('--s3path', type=str, help='Provide the s3 file path')
 
 
-args = parser.parse_args()
+args = parser.parse_args();
+path=args.s3path;
+stack_name = args.env + '-' + args.filename;
 
 client = boto3.client('cloudformation', region_name='us-east-1')
 
-def create_stack(stack_name,path):
+def create_stack(StackName,TemplateUrl):
     response = client.create_stack(
-    path=args.s3path,
-    stack_name = args.env + '-' + args.filename
+    TemplateUrl=path,
+    StackName=stack_name
     )
 
 
-def delete_stack(stack_name):
+def delete_stack(StackName,TemplateUrl):
     response = client.delete_stack(
-        stack_name=args.env + '-' + args.filename
-
+        TemplateUrl=path,
+        StackName=stack_name
     )
-def update_stack(stack_name,path):
+def update_stack(StackName,TemplateUrl):
     response = client.update_stack(
-        path=args.s3path,
-        stack_name=args.env + '-' + args.filename
+        TemplateUrl=path,
+        StackName=stack_name
     )
 
-def check_status(stack_name):
+def check_status(StackName,TemplateUrl):
     response = client.describe_stacks(
-        path=args.s3path,
-        stack_name=args.env + '-' + args.filename)
+        TemplateUrl=path,
+        StackName=stack_name)
     for StackStatus in stack_name:
         if StackStatus!='CREATE_COMPLETE':
             time.sleep(500)
