736fd6baf819ed83a362afde407599618646ae1d
shivakrishna <shivakrishna.ramadugu@gmail.com>
2017-12-22 18:40:37 -0600

    news stack

---
+++ Infrastructure/create-update-stack/create_update_stack.py.py
@@ -0,0 +1,19 @@
+import boto3
+import argparse
+
+parser = argparse.ArgumentParser(description='create update stack')
+parser.add_argument('--env', type=str, help='Provide the environment')
+parser.add_argument('--filename', type=str, help='Provide the filename')
+parser.add_argument('--s3path', type=str, help='Provide the s3 file path')
+
+args = parser.parse_args()
+stack_name=args.env+'-'+args.filename
+
+
+client = boto3.client('cloudformation')
+
+response = client.create_stack(
+    StackName=stack_name,
+    TemplateURL=args.s3path
+    )
+
