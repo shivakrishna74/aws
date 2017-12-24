94fa1a1feacabaf7467a47d4594e0336c5be76a7
shivakrishna <shivakrishna.ramadugu@gmail.com>
2017-12-24 14:04:22 -0600

    debug

--- Infrastructure/create-update-stack/create-update-stack.py
+++ Infrastructure/create-update-stack/create-update-stack.py
@@ -83,9 +83,10 @@ def main():
         if stack_operation == 'CREATE':
             create_stack(stackname, templateurl)
             check_stack_status(stackname, stack_chk_value='CREATE_COMPLETE')
-    except error as e:
-        logging.info("error :{0}".format(e.value))
-        if "AlreadyExistsException" in e.value:
+    except Exception as e:
+        pass
+        logging.info("error :{0}".format(e))
+        if "AlreadyExistsException" in e:
             logging.info("stack:{0} already exists".format(stackname))
             delete_stack(stackname)
             check_stack_status(stackname,stack_chk_value='DELETE_COMPLETE')
