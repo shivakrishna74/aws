560a750240d3a5b8b5b9d70c98944adf153dac7f
shivakrishna <shivakrishna.ramadugu@gmail.com>
2017-12-27 23:58:45 -0600

    debug

--- Infrastructure/create-update-stack/create-update-stack.py
+++ Infrastructure/create-update-stack/create-update-stack.py
@@ -45,6 +45,14 @@ def create_stack(stackname,path):
                 StackName=stackname)
             logging.info("Issued delete stack")
             check_stack_status(stackname,stack_chk_value='DELETE_COMPLETE')
+            logging.info("Creating Stack after the delete")
+            crt_response = client.create_stack(
+                TemplateURL=path,
+                StackName=stackname
+            )
+            logging.info("Verifying the stack status to create_complete")
+            check_stack_status(stackname, stack_chk_value='CREATE_COMPLETE')
+
 
 
         else:
